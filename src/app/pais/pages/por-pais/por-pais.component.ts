39f1a63228db5f813ce4273946fff637348eb9e5
potlitel <potlitel@gmail.com>
2023-02-03 13:16:18 -0500

    Se comienza con el consumo del api de paises.

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styleUrls: ['./por-pais.component.css']
+  styleUrls: ['./por-pais.component.css'],
 })
 export class PorPaisComponent {
+  //variable bindeada con el componente.html (se actualiza en ambas)
+  termino: string = '';
 
+
+  constructor( private paisService:PaisService ){}
+  
+  buscar() {
+    // console.log(this.termino);
+    this.paisService.buscarPais( this.termino ).subscribe( resp => {
+        console.log(resp);
+    } );
+  }
 }
---
+++ src/app/pais/services/pais.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PaisService {
+  // constante apiUrl para manejar la dirección del servicio
+  private apiUrl: string = 'https://restcountries.com/v2';
+  /*
+   * Constructor for Emp class
+   */
+  constructor(private httpClient: HttpClient) {}
+  /**
+   * Función para buscar paises (by name) dentro del servicio de paises.
+   * @author potlitel@gmail.com
+   * @version 1.0.0
+   * @return {Observable} Return a observable
+   **/
+  buscarPais(termino: string): Observable<any> {
+    const url = ` ${this.apiUrl}/name/${termino} `;
+    return this.httpClient.get(url);
+  }
+}
